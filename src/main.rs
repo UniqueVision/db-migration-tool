5a162dbbbea398ffc701ce145e45e6013deaac06
aoyagikouhei <aoyagi_kouhei@uniquevision.co.jp>
2020-07-28 19:09:56 +0900

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+#[macro_use]
+extern crate clap;
+use clap::{Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = app_from_crate!()
+        .arg(
+            Arg::with_name("url")
+            .short("u")
+            .long("url")
+            .help("postgresql url")
+            .required(true)
+            .takes_value(true)
+        )
+        .arg(Arg::with_name("patch_dirs")
+            .help("Patch Dir")
+            .short("p")
+            .long("patch_dir")
+            .multiple(true)
+            .takes_value(true)
+        )
+        .arg(Arg::with_name("stored_dirs")
+            .help("Stored Dir")
+            .short("s")
+            .long("stored_dir")
+            .multiple(true)
+            .takes_value(true)
+        ).get_matches();
+
+    let patch_dirs: Vec<_> = match matches.values_of("patch_dirs") {
+        Some(values) => values.collect(),
+        None => vec![]
+    };
+    let stored_dirs: Vec<_> = match matches.values_of("stored_dirs") {
+        Some(values) => values.collect(),
+        None => vec![]
+    };
+    println!("{:?}", patch_dirs);
 }
